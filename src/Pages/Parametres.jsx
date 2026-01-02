a0987334890ebae35dfbff966fcf67c115dd9982
Aymen HS <111710811+AymenMHS@users.noreply.github.com>
2026-01-02 22:38:59 +0100

    VQider complètement la branche

--- src/Pages/Parametres.jsx
+++
@@ -1,9 +0,0 @@
-// src/Pages/Parametres.jsx
-import React from "react";
-import SettingsLayout from "../components/settings/SettingsLayout";
-
-const Parametres = () => {
-  return <SettingsLayout />;
-};
-
-export default Parametres;
